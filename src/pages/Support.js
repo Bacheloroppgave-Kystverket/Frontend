7ad637901b355609beaec89bb622a8d612a203aa
Fereshta Ahmadi <fereshta@live.no>
2023-04-02 17:01:38 +0200

    Removed width from support button

--- src/pages/Support.js
+++ src/pages/Support.js
@@ -5,24 +5,39 @@ import NormalButton from "../components/openBridge/NormalButton";
 import SupportButton from "../components/openBridge/SupportButton";
 import "./support.css";
 
-
-
 export default function Support() {
-    return (
-        <div className="support-page">
-            <NormalTextField placeholder={"search"} style={{display: "flex", justifyContent:"center"}}/>
-            <div className="support-page-buttons">
-                <SupportButton text={"Eye tracking metrics"} description={"Introduction into the metrics"}/>
-                <SupportButton text={"Sessions"} description={"Basic introduction to sessions"}/>
-                <SupportButton text={"Profile"} description={"Information about your profile"}/>
-                <SupportButton text={"Profile"} description={"Information about your profile"}/>
-                <SupportButton text={"Graphs basic"} description={"Example of bargraphs"}/>
-                <SupportButton text={"Graphs basic"} description={"Example of bargraphs"}/>
-            </div>
-        </div>
-
-    );
-
-
-
+  return (
+    <div className="support-page">
+      <NormalTextField
+        placeholder={"search"}
+        style={{ display: "flex", justifyContent: "center" }}
+      />
+      <div className="support-page-buttons">
+        <SupportButton
+          text={"Eye tracking metrics"}
+          description={"Introduction into the metrics"}
+        />
+        <SupportButton
+          text={"Sessions"}
+          description={"Basic introduction to sessions"}
+        />
+        <SupportButton
+          text={"Profile"}
+          description={"Information about your profile"}
+        />
+        <SupportButton
+          text={"Profile"}
+          description={"Information about your profile"}
+        />
+        <SupportButton
+          text={"Graphs basic"}
+          description={"Example of bargraphs"}
+        />
+        <SupportButton
+          text={"Graphs basic"}
+          description={"Example of bargraphs"}
+        />
+      </div>
+    </div>
+  );
 }
